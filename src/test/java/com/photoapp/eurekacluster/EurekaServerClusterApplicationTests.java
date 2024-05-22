16ae0b5daa38096d594cd891b97e650eb00ba8b6
Swapnil Mane <maneswap1@gmail.com>
2024-05-22 12:33:49 +0530

    added new code

---
+++ src/test/java/com/photoapp/eurekacluster/EurekaServerClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.eurekacluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
